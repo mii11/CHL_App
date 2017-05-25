bf5c057e3b0b4aa2eede67bdf700e59d56a98c8f
mii11 <batida999@gmail.com>
2017-05-25 13:38:47 +0900

    Add code "PlayerController"

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speedX;
+	public float speedY;
+	public float speedZ;
+
+	void Update () {
+		float vertical = Input.GetAxis ("Vertical");
+		float horizontal = Input.GetAxis ("Horizontal");
+
+
+		//失敗		if (Input.GetKey ("up") == true) || (Input.GetKey ("down") == true) {
+		if (Input.GetKey ("up")) {
+			MoveForwardBaclward(vertical);
+		}
+
+		if (Input.GetKey ("down")) {
+			MoveForwardBaclward(vertical);
+		}
+
+		if (Input.GetKey ("right")) {
+			MoveRightLeft(horizontal);
+		}
+
+		if (Input.GetKey ("left")) {
+			MoveRightLeft(horizontal);
+		}
+
+
+	}
+
+	void MoveForwardBaclward(float vertical){
+		transform.Translate (0, 0, vertical * speedZ); 
+	}
+
+	void MoveRightLeft(float horizontal){
+		transform.Translate (horizontal * speedX, 0, 0);
+	}
+
+}
