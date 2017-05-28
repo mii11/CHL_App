f44925f846d0abf5f29e1aad4fc041d6e07e63ea
mii11 <batida999@gmail.com>
2017-05-28 22:21:30 +0900

    addded hp function again

--- Assets/Scripts/CoreController.cs
+++ Assets/Scripts/CoreController.cs
@@ -3,24 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CoreController : MonoBehaviour {
-	Animator anim;
-	public bool IsAttackCore;
-	//	Animation animation;
+	public GameObject enemyObj;
+	EnemyController enemyController;
 
-	// Use this for initialization
-	void Start () {
-		IsAttackCore = false;
-//		anim = GetComponent<Animator> ();	
+
+	void Start(){
+		//EnemyControllerクラスの変数にenemyオブジェクトのコンポーネントを代入する
+		enemyController = enemyObj.GetComponent<EnemyController> ();
+
 	}
-	
+
 	void OnTriggerEnter(Collider other){
-//		print ("CoreのColliderの中");
+		
+		print ("CoreのOnTrigger、other.gameObject.tagは" + other.gameObject.tag);
 
 		if (other.gameObject.tag == "PlayerBullet") {
 			if (gameObject.tag == "Core") {
 				print ("Core にあたった");
-				IsAttackCore = true;
-//				anim.SetBool ("IsAttackCore", true);
+				enemyController.curHp = 0;
+				enemyController.Die();
 			}
 		}
 
